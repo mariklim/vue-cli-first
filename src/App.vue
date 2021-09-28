aece11eb9395cb81859324ee20d75c66ed402955
mariklim <maria.klimenko889@gmail.com>
2021-09-28 15:48:12 +0200

    add hero

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Title msg="La mia prima app con Vue CLI!"/>
+    <Hero msg="https://static.vecteezy.com/ti/vettori-gratis/p1/2120554-upskill-learning-new-things-o-knowledge-development-for-new-skill-and-better-job-qualification-concept-vettoriale.jpg"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Title,
+    Hero,
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img :src= "msg">
+    </div>
+</template>
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+div img{
+    max-width:400px ;
+}
+ 
+</style>
